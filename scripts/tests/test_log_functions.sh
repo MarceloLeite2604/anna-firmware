9ff4f9cb93a870e6e237f319087d3b3741080490
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-02-06 19:20:40 -0200

    Modified "_log_write_log_message" on "log_functions.sh". When a log file is not specified, then the message should be redirected to "stderr".

--- scripts/tests/test_log_functions.sh
+++ scripts/tests/test_log_functions.sh
@@ -29,51 +29,6 @@ test_log_write_stderr(){
     echo -e "Tests of function \"_log_write_stderr\" concluded.\n";
 }
 
-# Test function "_log_write_on_file".
-test_log_write_on_file(){
-    echo "Testing function \"_log_write_on_file\".";
-
-    local readonly valid_test_file_path="../../tests/log/test_log_write_on_file.log";
-    local readonly invalid_test_file_path="../../tests/log/invalid_test_log_write_on_file.log";
-    local readonly no_access_test_file_path="/tmp/unaccessable_test_log_write_on_file.log";
-
-    if [ -f "${valid_test_file_path}" ];
-    then
-        echo "File \"${valid_test_file_path}\" already exists. It will be recreated.";
-        rm -f "${valid_test_file_path}";
-    fi;
-    touch "${valid_test_file_path}";
-
-    if [ -f "${invalid_test_file_path}" ];
-    then
-        echo "File \"${invalid_test_file_path}\" already exists. It will be recreated.";
-        rm -f "${invalid_test_file_path}";
-    fi;
-
-    if [ -f "${no_access_test_file_path}" ];
-    then
-        echo "File \"${no_access_test_file_path}\" already exists. It will be recreated.";
-        rm -f "${no_access_test_file_path}";
-    fi;
-    touch "${no_access_test_file_path}";
-    chmod u-w "${no_access_test_file_path}";
-
-    # Executes function without enough parameters.
-    _log_write_on_file;
-    _log_write_on_file $valid_test_file_path;
-
-    # Executes function writing on a valid file.
-    _log_write_on_file ${valid_test_file_path} "This message should be written correctly."
-
-    # Executes function writing on a invalid file.
-    _log_write_on_file ${invalid_test_file_path} "This message should not be written."
-
-    # Executes function on a existing file wich user does not have write permission.
-    _log_write_on_file ${no_access_test_file_path} "This message also should not be written.";
-
-    echo -e "Tests of function \"_log_write_on_file\" concluded.\n";
-}
-
 # Tests "set_log_directory" function.
 test_set_log_directory(){
     echo "Testing function \"set_log_directory\".";
@@ -132,6 +87,52 @@ test_set_log_directory(){
     echo -e "Tests of function \"set_log_directory\" concluded.\n";
 }
 
+# Test function "_log_write_on_file".
+test_log_write_on_file(){
+    echo "Testing function \"_log_write_on_file\".";
+
+    local readonly valid_test_file_path="../../tests/log/test_log_write_on_file.log";
+    local readonly invalid_test_file_path="../../tests/log/invalid_test_log_write_on_file.log";
+    local readonly no_access_test_file_path="/tmp/unaccessable_test_log_write_on_file.log";
+
+    if [ -f "${valid_test_file_path}" ];
+    then
+        echo "File \"${valid_test_file_path}\" already exists. It will be recreated.";
+        rm -f "${valid_test_file_path}";
+    fi;
+    touch "${valid_test_file_path}";
+
+    if [ -f "${invalid_test_file_path}" ];
+    then
+        echo "File \"${invalid_test_file_path}\" already exists. It will be recreated.";
+        rm -f "${invalid_test_file_path}";
+    fi;
+
+    if [ -f "${no_access_test_file_path}" ];
+    then
+        echo "File \"${no_access_test_file_path}\" already exists. It will be recreated.";
+        rm -f "${no_access_test_file_path}";
+    fi;
+    touch "${no_access_test_file_path}";
+    chmod u-w "${no_access_test_file_path}";
+
+    # Executes function without enough parameters.
+    _log_write_on_file;
+    _log_write_on_file $valid_test_file_path;
+
+    # Executes function writing on a valid file.
+    _log_write_on_file ${valid_test_file_path} "This message should be written correctly."
+
+    # Executes function writing on a invalid file.
+    _log_write_on_file ${invalid_test_file_path} "This message should not be written."
+
+    # Executes function on a existing file wich user does not have write permission.
+    _log_write_on_file ${no_access_test_file_path} "This message also should not be written.";
+
+    echo -e "Tests of function \"_log_write_on_file\" concluded.\n";
+}
+
+
 # Test "_log_create_log_file_name" function.
 test_log_create_log_file_name(){
     echo "Testing function \"_log_create_log_file_name\".";
@@ -141,7 +142,7 @@ test_log_create_log_file_name(){
     _log_create_log_file_name a b;
 
     # Executes function with valid parameters.
-    local readonly temporary_log_file_name="$(_log_create_log_file_name "test")";
+    local readonly temporary_log_file_name="$(_log_create_log_file_name "test_log_create_log_file_name")";
     echo "Log file name created: \"${temporary_log_file_name}\".";
 
     echo -e "Tests of function \"_log_create_log_file_name\" concluded.\n";
@@ -156,7 +157,7 @@ test_create_log_file(){
     create_log_file "invalid" "test";
 
     # Executes "create_log_file" function with valid parameters.
-    create_log_file "test_log_functions";
+    create_log_file "test_create_log_file";
     echo -e "\nChecking log file location.";
     ls -la "$(get_log_directory)";
     echo -e "Done.\n";
@@ -164,7 +165,7 @@ test_create_log_file(){
     # Executes "finish_log_file" with invalid parameters.
     finish_log_file "invalid parameter";
 
-    local readonly temporary_log_file_location=$(get_log_path);
+    local readonly temporary_log_file_path=$(get_log_path);
 
     # Executes "finish_log_file" with valid parameters. Also, a log file should be open, so this execution will close it.
     finish_log_file;
@@ -173,15 +174,15 @@ test_create_log_file(){
     finish_log_file;
 
     # Checks log file content.
-    echo -e "\nChecking log file \"${temporary_log_file_location}\" content.";
-    cat ${temporary_log_file_location};
+    echo -e "\nChecking log file \"${temporary_log_file_path}\" content.";
+    cat ${temporary_log_file_path};
     echo -e "Done.\n";
 
     echo -e "Tests of functions \"create_log_file\" and \"finish_log_file\" concluded.\n";
 }
 
 # Test "_log_write_log_message" function.
-test_write_log_message(){
+test_log_write_log_message(){
     echo "Testing function \"_log_write_log_message\".";
 
     create_log_file "test_log_write_log_message";
@@ -194,13 +195,16 @@ test_write_log_message(){
     # Executes function with valid parameters.
     _log_write_log_message "This message should be written on log file."
 
-    local readonly temporary_log_file_location=${log_file_location};
+    local readonly temporary_log_file_path=$(get_log_path);
 
     finish_log_file;
 
-    echo -e "\nChecking content of file \"${temporary_log_file_location}\".";
-    cat ${temporary_log_file_location};
+    echo -e "\nChecking content of file \"${temporary_log_file_path}\".";
+    cat ${temporary_log_file_path};
     echo -e "End of content.\n";
+
+    # Executes the function again, but, once there is no log file specified, the message must be redirected to "stderr".
+    _log_write_log_message "This message should be written on stderr."
 
 
     echo -e "Tests of functions \"_log_write_log_message\" concluded.\n";
@@ -246,12 +250,12 @@ test_set_log_level(){
     echo "Returning to original log level.";
     set_log_level ${original_log_level};
     
-    local readonly temporary_log_file_location=$(get_log_path);
+    local readonly temporary_log_file_path=$(get_log_path);
 
     finish_log_file;
 
-    echo -e "\nChecking content of file \"${temporary_log_file_location}\".";
-    cat ${temporary_log_file_location};
+    echo -e "\nChecking content of file \"${temporary_log_file_path}\".";
+    cat ${temporary_log_file_path};
     echo -e "End of content.\n";
 
     echo -e "Tests of functions \"set_log_level\", \"log\" and \"trace\" concluded.\n";
@@ -263,4 +267,5 @@ test_set_log_directory;
 test_log_write_on_file;
 test_log_create_log_file_name;
 test_create_log_file;
+test_log_write_log_message;
 test_set_log_level;
