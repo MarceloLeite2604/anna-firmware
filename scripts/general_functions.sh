4b92bd59d3881a8e059f32a01d965d6640d37d8e
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-02-07 13:58:16 -0200

    Added "check_file_exists", "check_write_permission", "check_read_permission" and "check_file_is_directory" functions on "general_functions.sh".

--- scripts/general_functions.sh
+++ scripts/general_functions.sh
@@ -49,40 +49,126 @@ get_current_time_formatted() {
     return ${success};
 };
 
-
-# Check if a directory exists and current user has read and write permissions on it.
+# Check if a file exists.
 #
 # Parameters
-#    1. Directory to be checked.
+#    1. File to be checked.
 #
 # Returns
-#   0. If directory exists and user has read and write permissions on it.
-#   1. Otherwise.
-check_directory_permissions(){
+#    0. If file exists.
+#    1. Otherwise
+check_file_exists(){
 
     if [ ${#} -ne 1 ];
     then
-        log ${log_message_type_error} "Invalid parameters to execute \"${FUNCNAME[1]}\".";
+        log ${log_message_type_error} "Invalid parameters to execute \"${FUNCNAME[0]}\".";
         return ${general_failure};
     fi;
 
-    local readonly directory_path="$1";
+    local readonly file_path="$1";
 
-    if [ ! -d "${directory_path}" ];
+    if [ ! -d "${file_path}" -a ! -f "${file_path}" ];
     then
-        log ${log_message_type_trace} "Path \"${directory_path}\" is invalid or is not a directory.";
+        log ${log_message_type_trace} "File \"${file_path}\" does not exist.";
         return ${general_failure};
     fi;
 
-    if [ ! -r ${directory_path} ];
+    return ${success};
+}
+
+# Check if current user has write permission on a file.
+#
+# Parameters
+#   1. File to be checked.
+#
+# Returns
+#   0. If user has write permission on file.
+#   1. Otherwise.
+check_write_permission(){
+
+    if [ ${#} -ne 1 ];
     then
-        log ${log_message_type_trace} "User \"$(whoami)\" does not have read permission on directory \"${directory_path}\".";
+        log ${log_message_type_error} "Invalid parameters to execute \"${FUNCNAME[0]}\".";
         return ${general_failure};
     fi;
 
-    if [ ! -w ${directory_path} ];
+    local readonly file_path="$1";
+
+    check_file_exists "${file_path}";
+    if [ $? -ne ${success} ];
     then
-        log ${log_message_type_trace} "User \"$(whoami)\" does not have write permission on directory \"${directory_path}\".";
+        return ${general_failure};
+    fi;
+
+    if [ ! -w "${file_path}" ];
+    then
+        log ${log_message_type_trace} "User \"$(whoami)\" does not have write permission on \"${file_path}\".";
+        return ${general_failure};
+    fi;
+
+    return ${success};
+}
+
+# Check if current user has read permission on a file.
+#
+# Parameters
+#   1. File to be checked.
+#
+# Returns
+#   0. If user has read permission on file.
+#   1. Otherwise.
+check_read_permission(){
+
+    if [ ${#} -ne 1 ];
+    then
+        log ${log_message_type_error} "Invalid parameters to execute \"${FUNCNAME[0]}\".";
+        return ${general_failure};
+    fi;
+
+    local readonly file_path="$1";
+
+    check_file_exists "${file_path}";
+    if [ $? -ne ${success} ];
+    then
+        return ${general_failure};
+    fi;
+
+    if [ ! -r "${file_path}" ];
+    then
+        log ${log_message_type_trace} "User \"$(whoami)\" does not have read permission on \"${file_path}\".";
+        return ${general_failure};
+    fi;
+
+    return ${success};
+}
+
+# Check if a file is a directory
+#
+# Parameters
+#    1. File to be checked.
+#
+# Returns
+#    0. If file is a directory.
+#    1. Otherwise
+check_file_is_directory(){
+
+    if [ ${#} -ne 1 ];
+    then
+        log ${log_message_type_error} "Invalid parameters to execute \"${FUNCNAME[0]}\".";
+        return ${general_failure};
+    fi;
+
+    local readonly file_path="$1";
+
+    check_file_exists "${file_path}";
+    if [ $? -ne ${success} ];
+    then
+        return ${general_failure};
+    fi;
+
+    if [ ! -d "${file_path}" ];
+    then
+        log ${log_message_type_trace} "File \"${file_path}\" is not a directory.";
         return ${general_failure};
     fi;
 
--- scripts/tests/test_general_functions.sh
+++ scripts/tests/test_general_functions.sh
@@ -38,16 +38,13 @@ test_get_current_time_formatted(){
     echo -e "Tests of function \"get_current_time_formatted\" concluded.\n";
 }
 
-# Tests "check_directory_permissions" function.
-test_check_directory_permissions(){
-    echo "Testing function \"check_directory_permissions\".";
+# Tests "check_file_exists" function.
+test_check_file_exists(){
+    echo "Testing function \"check_file_exists\".";
 
-    # local readonly root_test_directory="$(dirname ${BASH_SOURCE})/../../tests/check_directory_permissions/";
-    local readonly root_test_directory="/tmp/projeto-anna/tests/check_directory_permissions/";
-
-    local readonly invalid_directory="${root_test_directory}tests/invalid_directory/";
-    local readonly unwritable_directory="${root_test_directory}unwritable_directory/";
-    local readonly unreadable_directory="${root_test_directory}unreadable_directory/";
+    local readonly root_test_directory="/tmp/projeto-anna/tests/check_file_exists/";
+    local readonly valid_file="${root_test_directory}valid_file";
+    local readonly invalid_file="${root_test_directory}invalid_file";
 
     if [ ! -d "${root_test_directory}" ];
     then
@@ -58,52 +55,185 @@ test_check_directory_permissions(){
         echo "Directory \"${root_test_directory}\" already exists.";
     fi;
 
-    if [ ! -d "${unwritable_directory}" ];
+    if [ ! -f "${valid_file}" ];
     then
-        echo "Directory \"${unwritable_directory}\" does not exist.";
-        mkdir -p "${unwritable_directory}";
-        echo "Directory \"${unwritable_directory}\" created.";
+        echo "File \"${valid_file}\" does not exist.";
+        touch "${valid_file}";
+        echo "File \"${valid_file}\" created.";
     else
-        echo "Directory \"${unwritable_directory}\" already exists.";
+        echo "File \"${valid_file}\" already exists.";
     fi;
-
-    chmod u-w "${unwritable_directory}";
-
-    if [ ! -d "${unreadable_directory}" ];
-    then
-        echo "Directory \"${unreadable_directory}\" does not exist.";
-        mkdir -p "${unreadable_directory}";
-        echo "Directory \"${unreadable_directory}\" created.";
-    else
-        echo "Directory \"${unreadable_directory}\" already exists.";
-    fi;
-
-    chmod u-r "${unreadable_directory}";
 
     # Executes function with invalid parameters.
-    check_directory_permissions;
-    check_directory_permissions invalid parameters;
+    check_file_exists;
+    check_file_exists invalid parameters;
 
     # Executes function with valid parameters.
-    check_directory_permissions "${root_test_directory}";
+    check_file_exists ${valid_file};
+    echo "Check a valid file returned \"$?\".";
 
-    # Executes function with valid parameters, but an invalid path
-    check_directory_permissions "this is not a valid path";
+    check_file_exists ${invalid_file};
+    echo "Check an invalid file returned \"$?\".";
 
-    # Executes function informing an invalid path.
-    check_directory_permissions "${invalid_directory}";
+    echo -e "Tests of function \"check_file_exists\" concluded.\n";
+}
 
-    # Executes function informing an unwritable directory.
-    check_directory_permissions "${unwritable_directory}";
+# Tests "check_write_permission" function.
+test_check_write_permission(){
+    echo "Testing function \"check_write_permission\".";
 
-    # Executes function informing an unreadable directory.
-    check_directory_permissions "${unreadable_directory}";
-    
-    echo -e "Tests of function \"check_directory_permissions\" concluded.\n";
+    local readonly root_test_directory="/tmp/projeto-anna/tests/check_write_permission/";
+    local readonly writeable_file="${root_test_directory}writeable_file";
+    local readonly unwriteable_file="${root_test_directory}unwriteable_file";
+
+    if [ ! -d "${root_test_directory}" ];
+    then
+        echo "Directory \"${root_test_directory}\" does not exist.";
+        mkdir -p "${root_test_directory}";
+        echo "Directory \"${root_test_directory}\" created.";
+    else
+        echo "Directory \"${root_test_directory}\" already exists.";
+    fi;
+
+    if [ ! -f "${writeable_file}" ];
+    then
+        echo "File \"${writeable_file}\" does not exist.";
+        touch "${writeable_file}";
+        echo "File \"${writeable_file}\" created.";
+    else
+        echo "File \"${writeable_file}\" already exists.";
+    fi;
+
+    if [ ! -f "${unwriteable_file}" ];
+    then
+        echo "File \"${unwriteable_file}\" does not exist.";
+        touch "${unwriteable_file}";
+        echo "File \"${unwriteable_file}\" created.";
+    else
+        echo "File \"${unwriteable_file}\" already exists.";
+    fi;
+
+    chmod u-w "${unwriteable_file}";
+
+    # Executes function with invalid parameters.
+    check_write_permission;
+    check_write_permission invalid parameters;
+
+    # Executes function with valid parameters.
+    check_write_permission ${writeable_file};
+    echo "Check a writeable file returned \"$?\".";
+
+    check_write_permission ${unwriteable_file};
+    echo "Check an unwriteable file returned \"$?\".";
+
+    echo -e "Tests of function \"check_write_permission\" concluded.\n";
+}
+
+# Tests "check_read_permission" function.
+test_check_read_permission(){
+    echo "Testing function \"check_read_permission\".";
+
+    local readonly root_test_directory="/tmp/projeto-anna/tests/check_read_permission/";
+    local readonly readable_file="${root_test_directory}readable_file";
+    local readonly unreadable_file="${root_test_directory}unreadable_file";
+
+    if [ ! -d "${root_test_directory}" ];
+    then
+        echo "Directory \"${root_test_directory}\" does not exist.";
+        mkdir -p "${root_test_directory}";
+        echo "Directory \"${root_test_directory}\" created.";
+    else
+        echo "Directory \"${root_test_directory}\" already exists.";
+    fi;
+
+    if [ ! -f "${readable_file}" ];
+    then
+        echo "File \"${readable_file}\" does not exist.";
+        touch "${readable_file}";
+        echo "File \"${readable_file}\" created.";
+    else
+        echo "File \"${readable_file}\" already exists.";
+    fi;
+
+    if [ ! -f "${unreadable_file}" ];
+    then
+        echo "File \"${unreadable_file}\" does not exist.";
+        touch "${unreadable_file}";
+        echo "File \"${unreadable_file}\" created.";
+    else
+        echo "File \"${unreadable_file}\" already exists.";
+    fi;
+
+    chmod u-r "${unreadable_file}";
+
+    # Executes function with invalid parameters.
+    check_read_permission;
+    check_read_permission invalid parameters;
+
+    # Executes function with valid parameters.
+    check_read_permission ${readable_file};
+    echo "Check a readable file returned \"$?\".";
+
+    check_read_permission ${unreadable_file};
+    echo "Check an unreadable file returned \"$?\".";
+
+    echo -e "Tests of function \"check_read_permission\" concluded.\n";
+}
+
+# Tests "check_file_is_directory" function.
+test_check_file_is_directory(){
+    echo "Testing function \"check_file_is_directory\".";
+
+    local readonly root_test_directory="/tmp/projeto-anna/tests/check_file_is_directory/";
+    local readonly directory="${root_test_directory}directory/";
+    local readonly file="${root_test_directory}file";
+
+    if [ ! -d "${root_test_directory}" ];
+    then
+        echo "Directory \"${root_test_directory}\" does not exist.";
+        mkdir -p "${root_test_directory}";
+        echo "Directory \"${root_test_directory}\" created.";
+    else
+        echo "Directory \"${root_test_directory}\" already exists.";
+    fi;
+
+    if [ ! -d "${directory}" ];
+    then
+        echo "Directory \"${directory}\" does not exist.";
+        mkdir -p "${directory}";
+        echo "Directory \"${directory}\" created.";
+    else
+        echo "Directory \"${directory}\" already exists.";
+    fi;
+
+    if [ ! -f "${file}" ];
+    then
+        echo "File \"${file}\" does not exist.";
+        touch "${file}";
+        echo "File \"${file}\" created.";
+    else
+        echo "File \"${file}\" already exists.";
+    fi;
+
+    # Executes function with invalid parameters.
+    check_file_is_directory;
+    check_file_is_directory invalid parameters;
+
+    # Executes function with valid parameters.
+    check_file_is_directory ${directory};
+    echo "Check a directory returned \"$?\".";
+
+    check_file_is_directory ${file};
+    echo "Check a file returned \"$?\".";
+
+    echo -e "Tests of function \"check_file_is_directory\" concluded.\n";
 }
 
 set_log_level ${log_message_type_trace};
 
 test_get_current_time;
 test_get_current_time_formatted;
-test_check_directory_permissions;
+test_check_file_exists;
+test_check_write_permission;
+test_check_read_permission;
+test_check_file_is_directory;
